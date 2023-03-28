4fc4cb1e61f552214b85b2b25612148d41eada01
chengxiang03 <chengxiang03@kuaishou.com>
2023-03-28 19:39:55 +0800

    feat: admin use UI component

---
+++ packages/lucky-admin/src/components/UIDemo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import { ref } from 'vue';
+import { MyButton } from 'lucky-ui-vite/dist/lucky-ui.mjs';
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <MyButton color="green">绿色按钮</MyButton>
+</template>
+
+<style lang="less" scoped></style>
--- packages/lucky-admin/src/main.ts
+++ packages/lucky-admin/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
+import 'lucky-ui-vite/dist/style.css';
+import LuckyUI from 'lucky-ui-vite/dist/lucky-ui.mjs';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(LuckyUI).mount('#app');
